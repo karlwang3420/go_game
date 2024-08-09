be7eeb8800ab6d286bce3e9bdc894b968d419af5
Karl Wang <karlwang3420@gmail.com>
2024-08-09 22:32:34 +0800

    refactor: Remove unnecessary code and fix package declaration in main.go and location.go files.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-<<<<<<< ours
 	"bufio"
 	"fmt"
 	"os"
@@ -20,31 +19,4 @@ func main() {
 
 	}
 
-}
-=======
-	"fmt"
-	"go_game/location"
-)
-
-func main() {
-	fmt.Println("Hello, World!")
-
-	loc := location.Location{Latitude: 37.7749, Longitude: -122.4194}
-
-	fmt.Println(loc.String())
-}
->>>>>>> theirs
-package main
-
-import (
-	"fmt"
-	"go_game/location"
-)
-
-func main() {
-	fmt.Println("Hello, World!")
-
-	loc := location.Location{Latitude: 37.7749, Longitude: -122.4194}
-
-	fmt.Println(loc.String())
 }
